94b4079f9b823255f6c2d13ff7793dabcd02b3a6
cris1972 <cris72@mail.ru>
2020-12-31 15:01:44 +0300

    button setting is work

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Button
+echo "# Button" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/cris1972/Button.git
+git push -u origin main
--- app/src/main/java/mag/langrage/buttond/MainActivity.java
+++ app/src/main/java/mag/langrage/buttond/MainActivity.java
@@ -2,6 +2,8 @@ package mag.langrage.buttond;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,7 +28,11 @@ public class MainActivity extends AppCompatActivity {
         btnSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setText("нажал кнопку настроек");
+                /* textView.setText("нажал кнопку настроек"); */
+                Context content = MainActivity.this;
+                Intent IntentSet= new Intent(content, Setting.class);
+                startActivity(IntentSet);
+
             }
         });
 
