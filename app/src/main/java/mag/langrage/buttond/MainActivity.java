e8d2920b4d724934d97d4d59f35251f7a1e73b24
cris1972 <cris72@mail.ru>
2021-01-07 12:31:44 +0300

    delete files is work

--- app/src/main/java/mag/langrage/buttond/MainActivity.java
+++ app/src/main/java/mag/langrage/buttond/MainActivity.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class MainActivity extends AppCompatActivity {
-      Button btnMain, btnSetting;
+
+    Button btnMain, btnSetting;
       TextView textView;
+      File myDir ;
+      String pathFileDelete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +31,44 @@ public class MainActivity extends AppCompatActivity {
         btnMain= findViewById(R.id.btnMain);
         btnSetting= findViewById(R.id.btnSetting);
         textView= findViewById(R.id.textView);
+        pathFileDelete=Environment.getExternalStorageDirectory().getAbsolutePath();
+        pathFileDelete=pathFileDelete+"/Telegram";
+        textView.setText(pathFileDelete);
+
         btnMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setText("нажал главную кнопку");
+
+             try {
+
+                 myDir = new File(pathFileDelete);
+                 deleteFolderRecursive(myDir);
+                 textView.setText("готово");
+
+
+             } catch (Exception e){
+                 textView.setText("U have a problams   "+e.getMessage());
+
+             }
+
+
             }
+
+            private void deleteFile() {
+
+                try {
+
+                    new File(pathFileDelete).createNewFile();
+                    textView.setText("готово");
+                } catch (Exception e) {
+                    textView.setText("U have a problams  deleteFile "+e.getMessage());
+                     } }
+
         });
         btnSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /* textView.setText("нажал кнопку настроек"); */
+
                 Context content = MainActivity.this;
                 Intent IntentSet= new Intent(content, Setting.class);
                 startActivity(IntentSet);
@@ -36,7 +76,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        
+
 
     }
 
+    private void deleteFolderRecursive(File dir) {
+
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    deleteFolderRecursive(file);
+                } else {
+                    file.delete();
+                }
+            }
+        }
+
+        dir.delete();
+    }
 }
+
